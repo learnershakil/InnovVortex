39708810224221d64eb4400e6b70a543fea55a6c
learnershakil <learnershakil@gmail.com>
2025-03-22 15:58:18 +0530

    Landing page done

--- components/product-section.tsx
+++ components/product-section.tsx
@@ -58,7 +58,7 @@ export default function ProductSection({ title, description, type }: ProductSect
         viewport={{ once: true }}
         className="text-center mb-12"
       >
-        <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+        <h2 className="text-3xl md:text-4xl font-bold text-white mb-4"  id="products">
           <span className="text-[#00E8FC]">{title}</span>
         </h2>
         <p className="text-gray-300 max-w-2xl mx-auto">{description}</p>
